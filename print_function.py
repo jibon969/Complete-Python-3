127e4c6e799abb49481b84560e8e0d447b222ec5
Jibon Ahmed <jayed.swe@gmail.com>
2021-01-01 11:03:49 +0600

    added print function

---
+++ print_function.py
@@ -0,0 +1,27 @@
+
+# print() function
+# Use to print something on screen
+
+"""
+String -> Collection of charaxter inside "Double quotes" or 'single quotes'
+"""
+
+# Example
+print("Hello World !")
+print('Hello World')
+
+
+# You can use 'single quotes' inside "Double quotes"
+print('Hello "Python" World')
+
+
+# You can use "Double quotes" inside 'single quotes'
+print("Hello 'Python' world")
+
+# You can not use
+"""
+print('I 'am Jibon')
+"""
+
+# Example
+print("I 'm Jibon Ahmed")
