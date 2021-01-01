b9373bb2477544cc69ea28789609cacc8314f46c
Jibon Ahmed <jayed.swe@gmail.com>
2021-01-01 20:53:02 +0600

    solve some escape sequence problem

--- 02_escape_sequences.py
+++ 02_escape_sequences.py
@@ -26,3 +26,28 @@ print("Hello \t World")
 
 # Example
 print("Hello \b World!")
+
+# output : Line A \n Line B
+print("Line A\\n Line B")
+print("Line A\\t Line B")
+print("This is 2 backslash \\\\")
+
+print("This is 4 backslash \\\\\\\\")
+
+# ************ print these following lines *****************
+# 1. this is \\ double backslash
+# 2. these are /|/\/\/\ mounains
+# 3. he is      awesome (use escape sequence instead ofmanual spaces )
+# 4. \" \n \t \' (print this is an output)
+
+# Example 1
+print("this is \\\\ double backslash")
+
+# Example 2
+print("these are /\\/\\/\/\\ mounains")
+
+# Example 3
+print("he is\tawesome")
+
+# Example
+print("\\\" \\n \\t \\'")
