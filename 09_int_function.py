69243a83406ea7ec7f65cfba1a553a2e42cb645a
Jibon Ahmed <jayed.swe@gmail.com>
2021-01-02 16:17:40 +0600

    added int function

---
+++ 09_int_function.py
@@ -0,0 +1,15 @@
+
+# int () function
+
+# Example 1
+first_number = int(input("Enter your first number : "))
+second_number = int(input("Enter your second number : "))
+total = first_number + second_number
+print(total)
+
+# Example 2
+number1 = str(4)
+number2 = float("44")
+number3 = int("44")
+print(type(number2))
+print(number2+number3)
